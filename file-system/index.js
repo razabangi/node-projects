e30b864f316a02b8d4de5532b79f8301f81e5e6d
razabangi <muhammadrazabangi9@gmail.com>
2023-08-13 02:00:29 +0500

    intial commit

---
+++ file-system/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+// const filename = Math.round(Math.random() * 1000);
+// const filename = "info";
+// const rename = "information";
+// const folderName = 'it-department';
+
+// =========== Make Directory ===============
+// fs.mkdirSync(folderName);
+
+// =========== Write Content In The File ===============
+// fs.writeFileSync(`it-department/${filename}.txt`, 'This is my first written file.');
+
+// =========== Append Content In The File ===============
+// fs.appendFileSync(`it-department/${filename}.txt`, 'This is my first written file.');
+
+// =========== Rename The File Name ===============
+// fs.renameSync(`it-department/${filename}.txt`, `it-department/${rename}.txt`)
+
+// =========== Read Content From The File ===============
+// const data = fs.readFileSync(`it-department/${rename}.txt`, 'utf-8');
+// console.log(data);
+
+// =========== Delete The File ===============
+// fs.unlinkSync(`it-department/${rename}.txt`);
+
+// =========== Delete The Directory ===============
+// fs.rmdirSync(folderName);
